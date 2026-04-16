42ea04581c476762c49ff5c9f66570039da182d3
brayan <brayantigreros2021@gmail.com>
2026-04-15 23:20:19 -0500

    ETL pipeline local correcto, incio del DAGA, extraccion por ahora y correcta

--- dags/etl_pipeline.py
+++ dags/etl_pipeline.py
@@ -5,15 +5,15 @@ import pandas as pd
 import os
 
 # Paths
-RAW_DATA_PATH = r'C:\Users\btigr\Documents\UAO\5\ETL\ETL_2026_1\proyecto\etl_project_ods-main\etl_project_ods-main\data'
-TEMP_CSV = "/opt/airflow/data/spotify_processing.csv"
-TEMP_API = "/opt/airflow/data/gbif_processing.csv"
+RAW_DATA_PATH = '/opt/airflow/data/incautaciones.csv'
+RAW_DATA_API = '/opt/airflow/data/gbif_raw.csv'
 
 # Define the DAG using the @dag decorator
 @dag(
     dag_id="proyecto_etl_2",
     start_date=datetime(2024, 1, 1),
     catchup=False,
+    schedule="@daily",
     tags=["etl", "proyecto", "datawarehouse", "incautaciones", "ods"],
     max_active_runs=1
 )
@@ -24,17 +24,17 @@ def etl_pipeline():
     @task()
     def extract_csv():
         df_incautaciones = pd.read_csv(RAW_DATA_PATH)
-        return TEMP_CSV
-    
+        return df_incautaciones
+
+
     @task()
     def extract_api():
-        df_gbif = extract_api(RAW_DATA_PATH)
-        return TEMP_API
-
-
+        df_api = pd.read_csv(RAW_DATA_API)
+        return df_api
+    
 
 # Execution order
-    df_incautaciones = extract_csv()
-    df_gbif = extract_api()
+    df_incautacion_csv = extract_csv()
+    df_api = extract_api()
 
 etl_pipeline()
